3c2c156aef5880cdfbc9b4d97a7606ad8dcdbdd0
Swapnil Kumawat <swapnil.kumawat@zingoy.com>
2021-01-31 00:22:06 +0530

    Added French Language yml and ContactController#create

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,17 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:notice] = t :message_sent_successfully
+    end
+    redirect_to contacts_path
+  end
 
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email, :mobile_number, :message)
   end
 
 end
